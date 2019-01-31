5af095c77e3cac43fc1607f35d70621f579e00b5
eloypellegrino <eloypellegrino@hotmail.com>
2019-01-31 10:04:36 -0200

    Iníco do desenvolvimento do menu mobile

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const background = document.querySelectorAll("body, .containerHeader");
 const backgroundSecondary = document.querySelectorAll(".about, .containerProject");
 const backgroundImage = document.querySelector(".iphoneContainer");
 const text = document.querySelectorAll("h1, h2, h3, h4, p, a");
+const menuMobile = document.querySelector(".menuMobile i");
+const menuMobileList = document.querySelector(".list");
+const itemMenu = document.querySelectorAll(".itemMenu");
 
 checkbox.addEventListener("click", () => {
     for (item of background) {
@@ -20,3 +23,22 @@ checkbox.addEventListener("click", () => {
     backgroundImage.classList.toggle("darkBackgroundImage");
 });
 checkbox.checked = false;
+
+function menuClick() {
+    if (menuMobile.classList.contains("fa-bars")) {
+        menuMobile.classList.replace("fa-bars", "fa-times");
+    } else {
+        menuMobile.classList.replace("fa-times", "fa-bars");
+    }
+    menuMobileList.classList.toggle("-active");
+}
+
+menuMobile.addEventListener("click", menuClick);
+
+itemMenu.forEach((item) => {
+    item.addEventListener("click", () => {
+        menuMobileList.classList.toggle("-active");
+        menuMobileList.classList.toggle("list closeMenuMobile");
+        menuMobile.classList.replace("fa-times", "fa-bars");
+    });
+});
